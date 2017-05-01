687a747550b951932bca95c94229bcae48e953ed
USER <user@USERs-iMac.local>
2017-05-01 19:50:26 +1000

    application created

---
+++ Utility Application/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Utility Application
+//
+//  Created by USER on 1/5/17.
+//  Copyright © 2017 214126186. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
