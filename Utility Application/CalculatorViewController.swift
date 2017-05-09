7a4a045365c79abb384821748175637b8d84837a
USER <user@USERs-iMac.local>
2017-05-09 19:14:59 +1000

    Calculator functions implemented

--- Utility Application/CalculatorViewController.swift
+++ Utility Application/CalculatorViewController.swift
@@ -20,5 +20,79 @@ class CalculatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var calculate = false
+    var displayNumber:Double = 0;
+    var lastNumber:Double = 0;
+    var function = 0;
     
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton)
+    {
+        if calculate == true
+        {
+            label.text = String(sender.tag-1)
+            displayNumber = Double(label.text!)!
+            calculate = false
+        }
+        else
+        {
+            label.text = label.text! + String(sender.tag-1)
+            displayNumber = Double(label.text!)!
+        }
+    }
+    
+    @IBAction func functions(_ sender: UIButton)
+    {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            lastNumber = Double(label.text!)!
+            
+            if sender.tag == 12 //division
+            {
+                label.text = "/";
+            }
+            else if sender.tag == 13 //multiplication
+            {
+                label.text = "*";
+            }
+            else if sender.tag == 14 //subtraction
+            {
+                label.text = "-";
+            }
+            else if sender.tag == 15 //addition
+            {
+                label.text = "+";
+            }
+            
+            function = sender.tag
+            calculate = true;
+        }
+        else if sender.tag == 16
+        {
+            if function == 12
+            {
+                label.text = String(lastNumber / displayNumber)
+            }
+            else if function == 13
+            {
+                label.text = String(lastNumber * displayNumber)
+            }
+            else if function == 14
+            {
+                label.text = String(lastNumber - displayNumber)
+            }
+            else if function == 15
+            {
+                label.text = String(lastNumber + displayNumber)
+            }
+        }
+        else if sender.tag == 11
+        {
+            label.text = "";
+            displayNumber = 0;
+            lastNumber = 0;
+            function = 0;
+        }
+    }
 }
