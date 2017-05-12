0c8f012c49094c4f3e4fdc4c358eb102a4c41953
USER <user@USERs-iMac.local>
2017-05-12 18:21:37 +1000

    Notes/to-do list functions implemented

--- Utility Application/NotesViewController.swift
+++ Utility Application/NotesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NotesViewController: UIViewController {
+class NotesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +20,41 @@ class NotesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var list = ["Your input gets put in here"]
     
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var listTableView: UITableView!
+    
+    @IBAction func AddButton(_ sender: Any)
+    {
+        if (textInput.text != "")
+        {
+            list.append(textInput.text!)
+            textInput.text = ""
+            listTableView.reloadData()
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            listTableView.reloadData()
+        }
+    }
+    
+
 }
